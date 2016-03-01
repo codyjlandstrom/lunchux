ec744de52cd965c9604a211543da52999cbd13c4
Cody J Landstrom <cody.landstrom@gmail.com>
2016-03-01 14:46:52 -0500

    push copy changes and layout tweaks

--- client/login.js
+++ client/login.js
@@ -3,20 +3,11 @@ Template.login.onRendered(function() {
 });
 
 Template.login.events({
-    // 'submit form': function(event){
-    //     event.preventDefault();
-    //     var email = $('[name=email]').val();
-    //     var password = $('[name=password]').val();
-    //     Meteor.loginWithPassword(email, password);
-    //     Router.go('/signature');
-    // },
     'submit form': function(event){
         event.preventDefault();
         var email = $('[name=email]').val();
         var password = $('[name=password]').val();
-        Accounts.createUser({
-            email: email,
-            password: password
-        });
+        Meteor.loginWithPassword(email, password);
+        Router.go('/signature');
     }
 });
