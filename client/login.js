7ce9c04136a847a2fbefe002cffa68e438306a7f
Cody J Landstrom <cody.landstrom@gmail.com>
2016-02-29 22:57:48 -0500

    add additional structure and styles for desktop

--- client/login.js
+++ client/login.js
@@ -1,12 +1,9 @@
-Template.register.events({
+Template.login.events({
     'submit form': function(event){
         event.preventDefault();
         var email = $('[name=email]').val();
         var password = $('[name=password]').val();
-        Accounts.createUser({
-            email: email,
-            password: password
-        });
+        Meteor.loginWithPassword(email, password);
         Router.go('/signature');
     }
 });
